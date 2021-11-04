77297a89d95e9d4b03c88b07191d7c1892acb0e8
The Horscht <realhorscht+github@gmail.com>
2021-11-04 23:54:19 +0100

    Fix collision trigger hitbox

--- files/respawn_statue/trigger_hit.lua
+++ files/respawn_statue/trigger_hit.lua
@@ -9,8 +9,9 @@ function collision_trigger(colliding_entity_id)
     GamePlaySound("data/audio/Desktop/event_cues.bank", "event_cues/orb/create", x, y)
     GlobalsSetValue("AdventureMode_respawn_x", x)
     GlobalsSetValue("AdventureMode_respawn_y", y)
-    for i, comp in ipairs(EntityGetComponentIncludingDisabled(entity_id, "ParticleEmitterComponent")) do
-      EntitySetComponentIsEnabled(entity_id, comp, true)
+    local parent = EntityGetParent(entity_id)
+    for i, comp in ipairs(EntityGetComponentIncludingDisabled(parent, "ParticleEmitterComponent")) do
+      EntitySetComponentIsEnabled(parent, comp, true)
     end
   end
 end

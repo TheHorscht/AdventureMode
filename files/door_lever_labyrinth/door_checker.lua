275d1b042f8bb330f4cb72a0a9b6e3488630776f
The Horscht <realhorscht+github@gmail.com>
2021-12-13 20:32:17 +0100

    Fix door labyrinth doors falling when doors get unloaded

--- files/door_lever_labyrinth/door_checker.lua
+++ files/door_lever_labyrinth/door_checker.lua
@@ -12,6 +12,31 @@ local active_state = tonumber(GlobalsGetValue("AdventureMode_leverdoor_puzzle_co
 local PhysicsBody_comp = EntityGetFirstComponentIncludingDisabled(entity_id, "PhysicsBodyComponent")
 if PhysicsBody_comp then
   local current_state = ComponentGetIsEnabled(PhysicsBody_comp)
+  local physics_joint_component = EntityGetFirstComponentIncludingDisabled(entity_id, "PhysicsJointComponent")
+  if not (physics_joint_component and current_state) then
+    local orientation = get_var_store_string(entity_id, "orientation")
+    local offsets = {
+      { x = 0, y = 0 },
+      { x = 0, y = 0 },
+    }
+    if orientation == "h" then
+      offsets[1].x = 6
+      offsets[1].y = 4
+      offsets[2].x = 17
+      offsets[2].y = 4
+    elseif orientation == "v" then
+      offsets[1].x = 4
+      offsets[1].y = 6
+      offsets[2].x = 4
+      offsets[2].y = 17
+    end
+    EntityAddComponent2(entity_id, "PhysicsJointComponent", {
+      nail_to_wall = true, breakable = false, grid_joint = true, pos_y = offsets[1].y, pos_x = offsets[1].x,
+    })
+    EntityAddComponent2(entity_id, "PhysicsJointComponent", {
+      nail_to_wall = true, breakable = false, grid_joint = true, pos_y = offsets[2].y, pos_x = offsets[2].x,
+    })
+  end
   local new_state = active_state ~= default_state
   if new_state ~= current_state then
     EntitySetComponentIsEnabled(entity_id, PhysicsBody_comp, new_state)
@@ -41,28 +66,5 @@ if PhysicsBody_comp then
       end
       EntitySetComponentIsEnabled(entity_id, sprite_particle_emitter_comp, false)
     end)
-
-    local orientation = get_var_store_string(entity_id, "orientation")
-    local offsets = {
-      { x = 0, y = 0 },
-      { x = 0, y = 0 },
-    }
-    if orientation == "h" then
-      offsets[1].x = 6
-      offsets[1].y = 4
-      offsets[2].x = 17
-      offsets[2].y = 4
-    elseif orientation == "v" then
-      offsets[1].x = 4
-      offsets[1].y = 6
-      offsets[2].x = 4
-      offsets[2].y = 17
-    end
-    EntityAddComponent2(entity_id, "PhysicsJointComponent", {
-      nail_to_wall = true, breakable = false, grid_joint = true, pos_y = offsets[1].y, pos_x = offsets[1].x,
-    })
-    EntityAddComponent2(entity_id, "PhysicsJointComponent", {
-      nail_to_wall = true, breakable = false, grid_joint = true, pos_y = offsets[2].y, pos_x = offsets[2].x,
-    })
   end
 end

9e6be8652215a0bdf61f1ca378f129a33e29d7bc
The Horscht <realhorscht+github@gmail.com>
2021-12-20 16:37:09 +0100

    Add option to levers to execute script instantly
    And make the doors in the lever maze toggle instantly to prevent stuck

--- files/lever.lua
+++ files/lever.lua
@@ -22,6 +22,30 @@ if not GetValueBool("initialized", false) then
   SetValueBool("initialized", true)
 end
 
+local function activate(entity_id)
+  local global_variable = get_var_store_string(entity_id, "global_variable")
+  local toggle_global_variables = get_var_store_string(entity_id, "toggle_global_variables")
+  local global_script_and_function = get_var_store_string(entity_id, "global_function")
+  local global_script, global_function
+  if global_script_and_function then
+    global_script, global_function = unpack(split_string(global_script_and_function, ";"))
+  end
+  -- It's pressed on the right and unpressed on the left position
+  local state = get_state()
+  local new_state = state.direction == 1
+  local lua_component = EntityGetFirstComponentIncludingDisabled(entity_id, "LuaComponent", "enable_on_lever_changed")
+  if lua_component then
+    EntitySetComponentIsEnabled(entity_id, lua_component, true)
+  end
+  SetValueBool("pressed", new_state)
+  if global_variable then
+    GlobalsSetValue(global_variable, new_state and "1" or "0")
+  end
+  if global_script then
+    SetTimeOut(0, global_script, global_function)
+  end
+end
+
 local state = get_state()
 local speed = 0.05
 if state.progress then
@@ -36,26 +60,9 @@ if state.progress then
   end
   EntitySetTransform(handle_entity, hx, hy, hrot)
   if state.progress == 1 then
-    local global_variable = get_var_store_string(entity_id, "global_variable")
-    local toggle_global_variables = get_var_store_string(entity_id, "toggle_global_variables")
-    local global_script_and_function = get_var_store_string(entity_id, "global_function")
-    local global_script, global_function
-    if global_script_and_function then
-      global_script, global_function = unpack(split_string(global_script_and_function, ";"))
-    end
     state.progress = nil
-    -- It's pressed on the right and unpressed on the left position
-    local new_state = state.direction == 1
-    local lua_component = EntityGetFirstComponentIncludingDisabled(entity_id, "LuaComponent", "enable_on_lever_changed")
-    if lua_component then
-      EntitySetComponentIsEnabled(entity_id, lua_component, true)
-    end
-    SetValueBool("pressed", new_state)
-    if global_variable then
-      GlobalsSetValue(global_variable, new_state and "1" or "0")
-    end
-    if global_script then
-      SetTimeOut(0, global_script, global_function)
+    if not state.instant_activation then
+      activate(entity_id)
     end
   end
 end
@@ -77,5 +84,11 @@ function interacting(entity_who_interacted, entity_interacted, interactable_name
     state.direction = -1
   else
     state.direction = 1
+  end
+  -- on_lever_start
+  local instant_activation = get_var_store_bool(entity_interacted, "instant_activation")
+  state.instant_activation = instant_activation
+  if instant_activation then
+    activate(entity_interacted)
   end
 end
